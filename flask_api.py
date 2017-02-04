c32a1b02b10571036f5f85d28e03de97ad4e419f
Noor Fernandez <noor.k.fernandez@gmail.com>
2017-02-04 16:06:08 +0800

    Add files via upload

--- flask_api.py
+++ flask_api.py
@@ -23,8 +23,8 @@ def home():
 
 @app.route('/store', methods=['POST'])
 def create_store():
-    request_data = request.get_json() # this is how the requests module
-    new_store = {                     # accepts input for POST
+    request_data = request.get_json()
+    new_store = {
         'name': request_data['name'],
         'items':[]
     }
@@ -43,7 +43,6 @@ def get_store(name):
 def get_stores():
     return jsonify({'stores': stores})
 
-# POST /store/<string:name>/item {name:, price:}
 @app.route('/store/<string:name>/item', methods=['POST'])
 def create_store_item(name):
     request_data = request.get_json()
@@ -57,21 +56,12 @@ def create_store_item(name):
             return jsonify(new_item)
     return jsonify({'message': 'item not found'})
 
-# GET /store/<string:name>/item
 @app.route('/store/<string:name>/item')
 def get_store_item(name):
     for store in stores:
         if store['name'] == name:
             return jsonify({'items': store['items']})
     return jsonify({'message': 'store item not found'})
-
-@app.route('/store', methods=['DELETE'])
-def del_store(name):
-    store_del = request.delete()
-    for store in stores:
-        if store['name'] == name:
-            stores.remove(store_del[0])
-        return jsonify({'stores': stores})
 
 # the app is actually running on port 5000
 app.run(port=None)
